45d8ea36431762d82d73c9402a01e761907cbbd0
Gustavo Ferreira da Silva <112019221+Gust4avo@users.noreply.github.com>
2022-12-15 23:08:43 -0300

    Add files via upload

---
+++ Lista7_dowhile/Ex05/script.js
@@ -0,0 +1,13 @@
+var contadora= 0;
+var resultado= 0;
+
+while (contadora <= 15) {
+    var resultado= resultado * 3;
+
+    if(resultado == 0) {
+        var resultado= resultado + 1;
+    } 5
+
+    document.write("3 ^ " + contadora + " = " + resultado + "<br>");
+    contadora++
+}
